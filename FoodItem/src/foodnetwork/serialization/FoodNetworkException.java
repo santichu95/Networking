5be3d9d5735303546a32e9eb6b5c22bddbde1545
Santiago Andaluz Ruiz <Santiago_Andaluz_Ruiz@baylor.edu>
2016-08-25 22:04:19 -0500

    Stubs done

---
+++ FoodItem/src/foodnetwork/serialization/FoodNetworkException.java
@@ -0,0 +1,25 @@
+package foodnetwork.serialization;
+/** Exception for food network handling
+ * 
+ * @author Santiago Andaluz Ruiz
+ *
+ */
+public class FoodNetworkException extends java.lang.Exception {
+	
+	/**Constructs food network exception with null cause
+	 * 
+	 * @param message exception message
+	 */
+	public FoodNetworkException(String message) {
+	}
+	
+	/**Constructs food network exception
+	 * 
+	 * @param message exception message
+	 * @param cause	exception cause
+	 */
+	public FoodNetworkException(String message, Throwable cause){
+		
+	}
+	
+}
