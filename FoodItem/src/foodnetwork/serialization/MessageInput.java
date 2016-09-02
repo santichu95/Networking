63cb34f582359b2383f086bf5254f9bed95cf200
Santiago Andaluz Ruiz <Santiago_Andaluz_Ruiz@baylor.edu>
2016-09-02 12:20:39 -0500

    A couple of tests left to write

--- FoodItem/src/foodnetwork/serialization/MessageInput.java
+++ FoodItem/src/foodnetwork/serialization/MessageInput.java
@@ -11,150 +11,161 @@ import java.io.IOException;
 
 /**
  * Deserialization input source
+ * 
  * @author Santiago Andaulz Ruiz
  *
  */
 public class MessageInput {
-	
 
-	java.io.InputStream input;
+    java.io.InputStream input;
+    final int charSize = 1;
+    byte[] buffer;
+    int charCount;
 
-	/**Constructs a new input source from an InputStream
-	 * 
-	 * @param in byte input source
-	 */
-	public MessageInput(java.io.InputStream in) {
-		input = in;
-	}
+    /**
+     * Constructs a new input source from an InputStream
+     * 
+     * @param in
+     *            byte input source
+     */
+    public MessageInput(java.io.InputStream in) {
+        input = in;
+    }
 
-	/**Reads in the Name of the FoodItem
-	 * 
-	 * @return The name of the FoodItem
-	 * @throws FoodNetworkException if expected more bytes than were given
-	 */
-	public String readName() throws FoodNetworkException {
-		int charCount;
-		byte[] length;
-		String name;
+    /**
+     * Reads in the Name of the FoodItem
+     * 
+     * @return The name of the FoodItem
+     * @throws FoodNetworkException
+     *             if expected more bytes than were given
+     */
+    public String readName() throws FoodNetworkException {
+        String name;
 
-		try{
-			charCount = this.readInt();
+        try {
+            charCount = this.readInt();
 
-			length = new byte[charCount];
-			
-			input.read(length, 0, charCount);
-			
-			name = new String(length);
-		}catch(IOException e) {
-			FoodNetworkException err = new FoodNetworkException("Expected to get name of food");
-			throw err;
-		}
-		
-		return name;
-	}
-	
-	/**Reads an int from InputStream
-	 * 
-	 * @return int that was read from InputStream
-	 * @throws FoodNetworkException if expected more bytes than were given
-	 */
-	public int readInt() throws FoodNetworkException {
-		int charCount;
-		byte[] length = new byte[1];
-		String tempInt = "";
-		boolean endInt = false;
-		
-		
-		try {
-			while(!endInt && 1 == input.read(length, 0, 1)) {
-				if ( length[0] <= '9' && length[0] >= '0' ) {
-					tempInt += ((char) length[0] );
-				}else {
-					endInt = true;
-				}
-			}
-		} catch (IOException e) {
-			FoodNetworkException err = new FoodNetworkException("Expected to get Character Count");
-			throw err;
-		}
-		
-		charCount = Integer.parseInt(tempInt);
-		return charCount;
-	}
+            buffer = new byte[charCount];
 
-	/**Reads the MealType from InputStream
-	 * 
+            input.read(buffer, 0, charCount);
+
+            name = new String(buffer);
+        } catch (IOException e) {
+            FoodNetworkException err = new FoodNetworkException("Expected to get name of food");
+            throw err;
+        }
+
+        return name;
+    }
+
+    /**
+     * Reads an int from InputStream
+     * 
+     * @return int that was read from InputStream
+     * @throws FoodNetworkException
+     *             if expected more bytes than were given
+     */
+    public int readInt() throws FoodNetworkException {
+        buffer = new byte[charSize];
+        String tempInt = "";
+        boolean endInt = false;
+
+        try {
+            while (!endInt && 1 == input.read(buffer, 0, charSize)) {
+                if (buffer[0] <= '9' && buffer[0] >= '0') {
+                    tempInt += ((char) buffer[0]);
+                } else {
+                    endInt = true;
+                }
+            }
+        } catch (IOException e) {
+            FoodNetworkException err = new FoodNetworkException("Expected to get Character Count");
+            throw err;
+        }
+
+        charCount = Integer.parseInt(tempInt);
+        return charCount;
+    }
+
+    /**
+     * Reads the MealType from InputStream
+     * 
      * @return MealType MealType read in
-	 * @throws FoodNetworkException if expected more bytes than were given
-	 */
-	public MealType readType() throws FoodNetworkException {
-		byte[] temp = new byte[1];
-		String convert;
-		MealType result;
-		
-		try {
-			input.read(temp, 0, temp.length);
-			convert = new String(temp);
-			result = MealType.getMealType(convert.charAt(0));
-		} catch (IOException e) {
-			FoodNetworkException err = new FoodNetworkException("Expected to get MealType Code");
-			throw err;
-		}
-		
-		return result;
-	}
+     * @throws FoodNetworkException
+     *             if expected more bytes than were given
+     */
+    public MealType readType() throws FoodNetworkException {
+        buffer = new byte[charSize];
+        String convert;
+        MealType result;
 
-	/**Reads the calories from InputStream
-	 * 
+        try {
+            input.read(buffer, 0, buffer.length);
+            convert = new String(buffer);
+            result = MealType.getMealType(convert.charAt(0));
+        } catch (IOException e) {
+            FoodNetworkException err = new FoodNetworkException("Expected to get MealType Code");
+            throw err;
+        }
+
+        return result;
+    }
+
+    /**
+     * Reads the calories from InputStream
+     * 
      * @return long calories
-	 * @throws FoodNetworkException if expected more bytes than were given
-	 */
-	public long readCal() throws FoodNetworkException {
-		
-		long cal;
-		byte[] length = new byte[1];
-		String tempLong = "";
-		boolean endLong = false;
-		
-		try {
-			while(!endLong && 1 == input.read(length, 0, 1)) {
-				if ( length[0] <= '9' && length[0] >= '0' ) {
-					tempLong += ((char) length[0] );
-				}else {
-					endLong = true;
-				}
-			}
-		} catch (IOException e) {
-			FoodNetworkException err = new FoodNetworkException("Expected to get Character Count");
-			throw err;
-		}
-		
-		cal = Long.parseLong(tempLong);
-		return cal;
-	}
-	
-    /**Reads the grams of fat form InputStream
-	 * 
-	 * @return grams of fat
-	 * @throws FoodNetworkException if more bytes were excpted than were recieved.
-	 */
-	public String readFat() throws FoodNetworkException {
-		byte[] length = new byte[1];
-		String fat = "";
-		boolean endInt = false;
-		
-		try {
-			while(!endInt && 1 == input.read(length, 0, 1)) {
-				if ( ( length[0] <= 'z' && length[0] >= 'a' ) || ( length[0] <= 'Z' && length[0] >= 'A' ) ) {
-					fat += ((char) length[0] );
-				}else {
-					endInt = true;
-				}
-			}
-		} catch (IOException e) {
-			FoodNetworkException err = new FoodNetworkException("Expected to get Character Count");
-			throw err;
-		}
-		return fat;
-	}
+     * @throws FoodNetworkException
+     *             if expected more bytes than were given
+     */
+    public long readCal() throws FoodNetworkException {
+
+        long cal;
+        buffer = new byte[charSize];
+        String tempLong = "";
+        boolean endLong = false;
+
+        try {
+            while (!endLong && 1 == input.read(buffer, 0, charSize)) {
+                if (buffer[0] <= '9' && buffer[0] >= '0') {
+                    tempLong += ((char) buffer[0]);
+                } else {
+                    endLong = true;
+                }
+            }
+        } catch (IOException e) {
+            FoodNetworkException err = new FoodNetworkException("Expected to get Character Count");
+            throw err;
+        }
+
+        cal = Long.parseLong(tempLong);
+        return cal;
+    }
+
+    /**
+     * Reads the grams of fat form InputStream
+     * 
+     * @return grams of fat
+     * @throws FoodNetworkException
+     *             if more bytes were excpted than were recieved.
+     */
+    public String readFat() throws FoodNetworkException {
+        buffer = new byte[charSize];
+        String fat = "";
+        boolean endFat = false;
+
+        try {
+            while (!endFat && 1 == input.read(buffer, 0, charSize)) {
+                if (((buffer[0] <= '9' && buffer[0] >= '0') || buffer[0] == '.')) {
+                    fat += ((char) buffer[0]);
+                } else {
+                    endFat = true;
+                }
+            }
+        } catch (IOException e) {
+            FoodNetworkException err = new FoodNetworkException("Expected to get Character Count");
+            throw err;
+        }
+        return fat;
+    }
 }
--- FoodItem/src/foodnetwork/serialization/MessageOutput.java
+++ FoodItem/src/foodnetwork/serialization/MessageOutput.java
@@ -12,54 +12,65 @@ import java.nio.ByteBuffer;
 
 /**
  * Serialization output source
+ * 
  * @author Santiago Andaulz Ruiz
  *
  */
 public class MessageOutput {
-	
-	java.io.OutputStream out;
-	
-	/**Constructs a new output source from an OutputStream
-	 * 
-	 * @param out byte output source
-	 */
-	public MessageOutput(java.io.OutputStream out){
-		this.out = out;
-	}
 
-	/**Writes name to OutputStream
-	 * 
-	 * @param name name 
-	 * @throws IOException
-	 */
-	public void writeName(String name) throws IOException {
-		out.write(name.getBytes());		
-	}
+    java.io.OutputStream out;
 
-	/**Writes calories to OutputStream
-	 * @param calories calories
-	 * @throws IOException
-	 */
-	public void writeCal(long calories) throws IOException {
-	    ByteBuffer buf = ByteBuffer.allocate(Long.BYTES);
-		buf.putLong(calories);
-		out.write(buf.array());
-		
-	}
+    /**
+     * Constructs a new output source from an OutputStream
+     * 
+     * @param out
+     *            byte output source
+     */
+    public MessageOutput(java.io.OutputStream out) {
+        this.out = out;
+    }
 
-	/**Writes meal type to OutputStream
-	 * @param type meal type
-	 * @throws IOException
-	 */
-	public void writeType(MealType type) throws IOException {
-		out.write(((Character) type.getMealTypeCode()).toString().getBytes());
-	}
+    /**
+     * Writes name to OutputStream
+     * 
+     * @param name
+     *            name
+     * @throws IOException
+     */
+    public void writeName(String name) throws IOException {
+        out.write(name.getBytes());
+    }
 
-	/**Writes Fat to OutputStream
-	 * @param fat grams of fat
-	 * @throws IOException
-	 */
-	public void writeFat(String fat) throws IOException {
-		out.write(fat.getBytes());	
-	}
+    /**
+     * Writes calories to OutputStream
+     * 
+     * @param calories
+     *            calories
+     * @throws IOException
+     */
+    public void writeCal(long calories) throws IOException {
+        out.write(((Long) calories).toString().getBytes());
+    }
+
+    /**
+     * Writes meal type to OutputStream
+     * 
+     * @param type
+     *            meal type
+     * @throws IOException
+     */
+    public void writeType(MealType type) throws IOException {
+        out.write(((Character) type.getMealTypeCode()).toString().getBytes());
+    }
+
+    /**
+     * Writes Fat to OutputStream
+     * 
+     * @param fat
+     *            grams of fat
+     * @throws IOException
+     */
+    public void writeFat(String fat) throws IOException {
+        out.write(fat.getBytes());
+    }
 }
